3c15e522f982b531f80cca7c9bd35ba639209780
Niklas Widmann <nik@somethingcatchy.net>
2026-07-13 21:24:30 +0200

    emit recipe graph

--- web/src/plugin/graph.ts
+++ web/src/plugin/graph.ts
@@ -1,14 +1,42 @@
 import type { AstroIntegration } from "astro";
+import { readdir, readFile } from "node:fs/promises";
+import { join } from "node:path";
 
-const script = `
+const graphsDir = join(import.meta.dirname, "..", "graph");
 
+const graphsTypes = await readdir(graphsDir);
+
+async function readGraph(name: string) {
+  const files = ["nodes.json", "edges.json"].map((it) =>
+    join(graphsDir, name, it),
+  );
+
+  const [nodes, edges] = await Promise.all(
+    files.map((it) => readFile(it).then((it) => JSON.parse(it.toString()))),
+  );
+
+  return { data: { nodes, edges }, files };
+}
+
+const entries = await Promise.all(
+  graphsTypes.map(async (name) => {
+    return [name, await readGraph(name)] as const;
+  }),
+);
+
+const graphs = Object.fromEntries(entries.map(([k, v]) => [k, v.data]));
+const files = entries.flatMap(([, v]) => v.files);
+
+const script = /* javascript */ `
+  window.graph = ${JSON.stringify(graphs)}
 `;
 
 export default <AstroIntegration>{
-  name: "htmx",
+  name: "graph",
   hooks: {
-    "astro:config:setup": ({ injectScript }) => {
-      injectScript("page", script);
+    "astro:config:setup": (astro) => {
+      astro.injectScript("page", script);
+      files.forEach((it) => astro.addWatchFile(it));
     },
   },
 };
