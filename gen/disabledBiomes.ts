c2383fe570cdbf02ff3e3035953bced3edfc6eab
Niklas Widmann <nik@somethingcatchy.net>
2026-07-16 10:09:39 +0200

    more end generation

--- gen/disabledBiomes.ts
+++ gen/disabledBiomes.ts
@@ -1,13 +1,18 @@
 const tags = loader.tags.scoped("minecraft:worldgen/biome");
 
-tags.empty("#has_structure/mineshaft_mesa");
 tags.empty("#has_structure/village_desert");
 tags.empty("#has_structure/village_plains");
 tags.empty("#has_structure/village_savanna");
 tags.empty("#has_structure/village_snowy");
 tags.empty("#has_structure/village_tiaga");
 tags.empty("#has_structure/woodland_mansion");
+tags.empty("#has_structure/mineshaft");
+tags.empty("#has_structure/mineshaft_mesa");
+tags.empty("#has_structure/stronghold");
+tags.empty("#has_structure/trial_chamber");
+tags.empty("#has_structure/trial_chambers");
+tags.empty("#nomansland:valid_sanctuary_biomes");
 
-tags.replace("#has_structure/mineshaft", ["#c:is_cave"]);
-tags.replace("#has_structure/stronghold", ["#c:is_cave"]);
-tags.replace("#has_structure/trial_chamber", ["#c:is_cave"]);
+// tags.replace("#has_structure/mineshaft", ["#c:is_cave"]);
+// tags.replace("#has_structure/stronghold", ["#c:is_cave"]);
+// tags.replace("#has_structure/trial_chamber", ["#c:is_cave"]);
